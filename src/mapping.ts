54e2b273bc29c4c94689101023f7c84cac9b9c2c
Conner Swenberg <cls364@cornell.edu>
2023-03-03 12:22:56 -0500

    Add basic total distributed accounting for split and members

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,12 @@
-import { Address, log } from "@graphprotocol/graph-ts";
-import { CreateSplit, CreateSplitCall } from "../generated/SplitMain/SplitMain";
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  CreateSplit,
+  CreateSplitCall,
+  DistributeETH,
+} from "../generated/SplitMain/SplitMain";
 import { Member, Split } from "../generated/schema";
+
+const PERCENTAGE_SCALE = 1_000_000;
 
 function genSplitId(proxyAddress: Address): string {
   return proxyAddress.toHex();
@@ -10,6 +16,15 @@ function joinIds(ids: string[]): string {
   return ids.join(":");
 }
 
+// reimplementation of internal function within SplitMain
+// https://github.com/0xSplits/splits-contracts/blob/main/contracts/SplitMain.sol#L808
+function _scaleAmountByPercentage(
+  amount: BigInt,
+  scaledPercent: BigInt
+): BigInt {
+  return amount.times(scaledPercent).div(BigInt.fromI64(PERCENTAGE_SCALE));
+}
+
 export function handleCreateSplit(event: CreateSplit): void {
   log.warning("handleCreateSplit", []);
   const splitId = genSplitId(event.params.split);
@@ -17,6 +32,7 @@ export function handleCreateSplit(event: CreateSplit): void {
   if (!split) {
     log.warning("handleCreateSplit: split does not yet exist", []);
     split = new Split(splitId);
+    split.members = [];
     split.save();
   }
 }
@@ -28,11 +44,13 @@ export function handleCreateSplitCall(call: CreateSplitCall): void {
   if (!split) {
     log.warning("handleCreateSplitCall: split does not yet exist", []);
     split = new Split(splitId);
-    split.save();
   }
+  split.distributorFee = call.inputs.distributorFee;
+  split.totalEthDistributed = BigInt.fromI32(0);
 
   let accounts = call.inputs.accounts;
   let allocations = call.inputs.percentAllocations;
+  let memberIds = new Array<string>();
 
   for (let i: i32 = 0; i < accounts.length; i++) {
     let accountId = accounts[i].toHex();
@@ -43,7 +61,45 @@ export function handleCreateSplitCall(call: CreateSplitCall): void {
     member.split = splitId;
     member.recipient = accountId;
     member.allocation = allocations[i];
+    member.totalEthDistributed = BigInt.fromI32(0);
     member.save();
+    memberIds.push(memberId);
     log.warning("handleCreateSplitCall: member saved", [memberId]);
+  }
+
+  split.members = memberIds;
+  split.save();
+}
+
+export function handleDistributeETH(event: DistributeETH): void {
+  let split = Split.load(genSplitId(event.params.split));
+  if (!split) {
+    log.warning(
+      "handleDistributeETH, no split found: " + genSplitId(event.params.split),
+      []
+    );
+    return;
+  }
+  let amountToSplit = event.params.amount;
+  split.totalEthDistributed = split.totalEthDistributed.plus(amountToSplit);
+  split.save();
+
+  const distributorFeeAmount = _scaleAmountByPercentage(
+    amountToSplit,
+    split.distributorFee
+  );
+  amountToSplit = amountToSplit.minus(distributorFeeAmount);
+
+  const memberIds = split.members;
+  for (let i: i32 = 0; i < memberIds.length; i++) {
+    let member = Member.load(memberIds[i]);
+    if (!member) {
+      log.warning("handleDistributeETH, missing member: " + memberIds[i], []);
+      return;
+    }
+    member.totalEthDistributed = member.totalEthDistributed.plus(
+      _scaleAmountByPercentage(amountToSplit, member.allocation)
+    );
+    member.save();
   }
 }
